06c1b31d0b22d5a481b69babb6f7c940c6a3132c
d00d171 <d00d171@gmail.com>
2014-07-14 14:06:47 +0200

    prettifying views, refactor for change info tab

--- app/src/main/java/pl/edu/agh/mobilecodereviewer/model/utilities/ChangeInfoHelper.java
+++ app/src/main/java/pl/edu/agh/mobilecodereviewer/model/utilities/ChangeInfoHelper.java
@@ -3,7 +3,6 @@ package pl.edu.agh.mobilecodereviewer.model.utilities;
 import com.google.common.base.Function;
 import com.google.common.collect.Lists;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -28,30 +27,30 @@ public final class ChangeInfoHelper {
         );
     }
 
-    public static Map<String, List<Pair<String, String>>> getChildren(List<ChangeInfo> changeInfoList, Map<ChildrenHeaders, String> childrenHeadersMap){
-        Map<String, List<Pair<String, String>>> childsMap = new HashMap<String, List<Pair<String, String>>>();
+    public static Map<String, Map<ChildrenHeaders, String>> getChildren(List<ChangeInfo> changeInfoList){
+        Map<String, Map<ChildrenHeaders, String>> childsMap = new HashMap<String, Map<ChildrenHeaders, String>>();
 
         for(ChangeInfo changeInfo : changeInfoList){
-            childsMap.put(changeInfo.getChangeId(), getChildContent(changeInfo, childrenHeadersMap));
+            childsMap.put(changeInfo.getChangeId(), getChildContent(changeInfo));
         }
 
         return childsMap;
     }
 
     private static Pair<String, String> getGroupHeader(ChangeInfo changeInfo){
-        return new Pair(changeInfo.getSubject(), changeInfo.getChangeId());
+        return new Pair(changeInfo.getChangeId(), changeInfo.getSubject());
     }
 
-    private static List<Pair<String, String>> getChildContent(ChangeInfo changeInfo, Map<ChildrenHeaders, String> childrenHeadersMap){
-        List<Pair<String, String>> childContent = new ArrayList<Pair<String, String>>();
+    private static Map<ChildrenHeaders, String> getChildContent(ChangeInfo changeInfo){
+        Map<ChildrenHeaders, String> childContent = new HashMap<ChildrenHeaders, String>();
 
-        childContent.add(new Pair(childrenHeadersMap.get(ChildrenHeaders.SUBJECT), changeInfo.getSubject()));
-        childContent.add(new Pair(childrenHeadersMap.get(ChildrenHeaders.STATUS), changeInfo.getStatus()));
-        childContent.add(new Pair(childrenHeadersMap.get(ChildrenHeaders.OWNER), changeInfo.getOwnerName()));
-        childContent.add(new Pair(childrenHeadersMap.get(ChildrenHeaders.PROJECT), changeInfo.getProject()));
-        childContent.add(new Pair(childrenHeadersMap.get(ChildrenHeaders.BRANCH), changeInfo.getBranch()));
-        childContent.add(new Pair(childrenHeadersMap.get(ChildrenHeaders.UPDATED), changeInfo.getUpdated()));
-        childContent.add(new Pair(childrenHeadersMap.get(ChildrenHeaders.SIZE), changeInfo.getSize().toString()));
+        childContent.put(ChildrenHeaders.SUBJECT, changeInfo.getSubject());
+        childContent.put(ChildrenHeaders.STATUS, changeInfo.getStatus());
+        childContent.put(ChildrenHeaders.OWNER, changeInfo.getOwnerName());
+        childContent.put(ChildrenHeaders.PROJECT, changeInfo.getProject());
+        childContent.put(ChildrenHeaders.BRANCH, changeInfo.getBranch());
+        childContent.put(ChildrenHeaders.UPDATED, changeInfo.getUpdated());
+        childContent.put(ChildrenHeaders.SIZE, changeInfo.getSize().toString());
 
         return childContent;
     }
