4ff506535947565e81f9cc24b346254c8fb22acb
d00d171 <d00d171@gmail.com>
2014-10-11 19:19:46 +0200

    singleton annotation for accountdao

--- app/src/main/java/pl/edu/agh/mobilecodereviewer/dao/gerrit/AccountDAOImpl.java
+++ app/src/main/java/pl/edu/agh/mobilecodereviewer/dao/gerrit/AccountDAOImpl.java
@@ -1,11 +1,14 @@
 package pl.edu.agh.mobilecodereviewer.dao.gerrit;
 
 
+import javax.inject.Singleton;
+
 import pl.edu.agh.mobilecodereviewer.dao.api.AccountDAO;
 import pl.edu.agh.mobilecodereviewer.dao.gerrit.tools.RestApi;
 import pl.edu.agh.mobilecodereviewer.dto.AccountInfoDTO;
 import pl.edu.agh.mobilecodereviewer.model.AccountInfo;
 
+@Singleton
 public class AccountDAOImpl implements AccountDAO{
 
     private RestApi restApi;
